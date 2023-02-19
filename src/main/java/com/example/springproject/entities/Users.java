884d74e216093a4d54872140397cfe06650d5b71
mehdifersi <mahdi.fersi@esprit.tn>
2023-02-19 23:45:36 +0100

    Base initiation

--- src/main/java/com/example/springproject/entities/Users.java
+++ src/main/java/com/example/springproject/entities/Users.java
@@ -1,4 +1,38 @@
 package com.example.springproject.entities;
 
-public class Users {
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table( name = "Users")
+public class Users implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idUser")
+    private Integer idUser; // Clé primaire
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String email;
+    private String password;
+    private String numTel;
+
 }
